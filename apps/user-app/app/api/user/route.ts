e15f5be84a7da4e38f7fcb1ae7a65424707ebece
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-04-17 19:01:13 +0530

    added session wrapper and server/ client side sessions

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,16 +1,20 @@
-import { NextResponse } from "next/server"
-import { PrismaClient } from "@repo/db/client";
-
-const client = new PrismaClient();
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../../../lib/auth";
 
 export const GET = async () => {
-    await client.user.create({
-        data: {
-            email: "dummy1@eazy.com",
-            name: "dummy eazy 1"
-        }
-    })
+  const session = await getServerSession(authOptions);
+  if (session.user) {
     return NextResponse.json({
-        message: "hi there"
-    })
-}
+      user: session.user,
+    });
+  }
+  return NextResponse.json(
+    {
+      message: "You are not logged in",
+    },
+    {
+      status: 403,
+    }
+  );
+};
