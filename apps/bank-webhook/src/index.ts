2cc5513ba4fb11e4ecfa3fbe190cd82cefdc4d68
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-04-18 10:04:53 +0530

    added bank-webook and transaction logic in BE app.

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,14 +1,49 @@
 import express from "express";
-import db from "@repo/db/client"
+import db from "@repo/db/client";
 
 const app = express();
 
-app.post("/hdfcWebhook", (req, res) => {
-    //TODO: Add zod validation here?
-    const paymentInformation = {
-        token: req.body.token,
-        userId: req.body.user_identifier,
-        amount: req.body.amount
-    };
-    // Update balance in db, add txn
-})
+app.post("/hdfcWebhook", async (req, res) => {
+  //TODO: Add zod validation here?
+  // TODO : Check the request exactly come from HDFC , by checking secret
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
+
+  try {
+    await db.$transaction([
+      // update the balance
+      db.balance.updateMany({
+        where: {
+          userId: Number(paymentInformation.userId),
+        },
+        data: {
+          amount: {
+            increment: Number(paymentInformation.amount),
+          },
+        },
+      }),
+
+      // update the transaction status
+      db.onRampTransaction.updateMany({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+
+    res.status(200).json({
+      message: "Captured",
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(411).json({
+      message: "Error while processing webhook",
+    });
+  }
+});
