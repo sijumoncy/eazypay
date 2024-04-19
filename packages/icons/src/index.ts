876cea4444da47710f0c916d9a4d3d4161021614
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-04-19 08:59:05 +0530

    created pages for dashboard and other menu pages; added icons in the shared package

---
+++ packages/icons/src/index.ts
@@ -0,0 +1,7 @@
+import Home from "./home";
+import Clock from "./clock";
+import TransferArrow from "./transferArrow";
+
+export { Home as HomeIcon };
+export { Clock as ClockIcon };
+export { TransferArrow as TransferArrowIcon };
---
+++ packages/icons/src/transferArrow.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+interface ITransferProps {
+  classes: string;
+}
+
+const TransferArrow: React.FC<ITransferProps> = ({ classes }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      id="transfer"
+      className={classes}
+      fill="current"
+    >
+      <g transform="matrix(0.83 0 0 0.83 12 12)">
+        <g style="">
+          <g transform="matrix(1 0 0 1 -4.1 -2.1)">
+            <path
+              style="stroke-dasharray: none; stroke-linecap: round; stroke-dashoffset: 0; stroke-linejoin: round; stroke-miterlimit: 4; fill: none; fill-rule: nonzero; opacity: 1;"
+              transform=" translate(-7.5, -9.5)"
+              d="M 0.5 9.5 L 14.5 9.5"
+              stroke-linecap="round"
+            />
+          </g>
+          <g transform="matrix(1 0 0 1 4.9 2.9)">
+            <path
+              style="stroke-dasharray: none; stroke-linecap: round; stroke-dashoffset: 0; stroke-linejoin: round; stroke-miterlimit: 4; fill: none; fill-rule: nonzero; opacity: 1;"
+              transform=" translate(-16.5, -14.5)"
+              d="M 9.5 14.5 L 23.5 14.5"
+              stroke-linecap="round"
+            />
+          </g>
+          <g transform="matrix(1 0 0 1 -9.1 -2.1)">
+            <path
+              style="stroke-dasharray: none; stroke-linecap: round; stroke-dashoffset: 0; stroke-linejoin: round; stroke-miterlimit: 4; fill: none; fill-rule: nonzero; opacity: 1;"
+              transform=" translate(-2.5, -9.5)"
+              d="M 4.5 13.5 L 0.5 9.5 L 4.5 5.5"
+              stroke-linecap="round"
+            />
+          </g>
+          <g transform="matrix(1 0 0 1 9.9 2.9)">
+            <path
+              style="stroke-dasharray: none; stroke-linecap: round; stroke-dashoffset: 0; stroke-linejoin: round; stroke-miterlimit: 4; fill: none; fill-rule: nonzero; opacity: 1;"
+              transform=" translate(-21.5, -14.5)"
+              d="M 19.5 18.5 L 23.5 14.5 L 19.5 10.5"
+              stroke-linecap="round"
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default TransferArrow;
