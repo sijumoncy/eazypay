22489130e783dff3f408bd8aba967c892973075d
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-05-28 21:51:16 +0530

    profile ui option changes

--- packages/ui/src/Profile/profile.tsx
+++ packages/ui/src/Profile/profile.tsx
@@ -18,17 +18,17 @@ interface IProfileProps {
 
 function profile({ avatarUrl, name, userId, dropdownItemsArr }: IProfileProps) {
   return (
-    <div className="w-full max-w-72">
+    <div className="w-full max-w-72 outline-none">
       <DropdownMenu>
-        <DropdownMenuTrigger className="w-full">
+        <DropdownMenuTrigger className="w-full outline-none">
           <ProfileCard name={name} avatarUrl={avatarUrl} userId={userId} />
         </DropdownMenuTrigger>
         {dropdownItemsArr?.length && (
-          <DropdownMenuContent className="max-w-full">
+          <DropdownMenuContent className="max-w-full bg-white">
             {dropdownItemsArr.map((sectionArr) => (
               <>
                 {sectionArr?.map((item) => (
-                  <DropdownMenuItem onClick={item.onClick}>
+                  <DropdownMenuItem onClick={item.onClick} className="hover:bg-gray-200 cursor-pointer">
                     {item.name}
                   </DropdownMenuItem>
                 ))}
